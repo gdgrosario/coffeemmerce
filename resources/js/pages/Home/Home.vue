3063c12647f4f2d092ec64a7e76d64e0fc81d094
Felipev98 <felipe_v_spd@hotmail.com>
2021-08-13 19:40:20 -0400

    styles: navbar,hero and footer section

--- resources/js/pages/Home/Home.vue
+++ resources/js/pages/Home/Home.vue
@@ -1,15 +1,19 @@
 <template>
-    <h1 class="text-xl font-semibold text-center">Welcome laravel with vue</h1>
-
-    <Count />
+<NavItem/>
+<Navbar/>
+<Hero/>
+<Footer/>
 </template>
 
 <script>
-import Count from "./components/Count.vue";
+import NavItem from '../Home/components/NavItem.vue'
+import Navbar from '../Home/components/Navbar.vue'
+import Hero from '../Home/components/Hero.vue'
+import Footer from '../Home/components/Footer.vue'
 export default {
     name: "Home",
     components: {
-        Count,
+        NavItem,Navbar,Hero,Footer
     },
 };
 </script>
