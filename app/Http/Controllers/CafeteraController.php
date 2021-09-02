b53d94502ed9acb6a92fb9881d8d02252eb0edfd
Elton Chavez <elton.mct@gmail.com>
2021-09-02 02:07:43 -0300

    update user controller with middleware / auth controller

--- app/Http/Controllers/CafeteraController.php
+++ app/Http/Controllers/CafeteraController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class CafeteraController extends Controller
 {
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:api');
+    // }
+
     public function index()
     {
         $cafeteras = Cafetera::all();
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,30 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Crypt;
-use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
     public function index()
     {
         $users = User::all();
-        return $users;
-        // return response()->json(["users"=>$users],200);
-    }
-
-    public function store(UserRequest $request)
-    {
-        $input = $request->all();
-        $input['password'] = Crypt::encrypt($input['password']);
-        $user = User::create($input);
-        return ['message' => 'User created', 'user' => $user];
+        return response()->json([
+            'ok'=> true,
+            'users' => $users
+        ], 200);
     }
 
     public function show($id)
     {
         $user = User::findOrFail($id);
-        return $user;
+        return response()->json([
+            'ok'=> true,
+            'user' => $user
+        ], 200);
     }
 
     public function update(Request $request, $id)
@@ -36,12 +36,19 @@ class UserController extends Controller
 
         $input = $request->all();
         $user->update($input);
-        return ['message' => 'User updated', 'user' => $user];
+        return response()->json([
+            'ok'=> true,
+            'message' => 'User updated',
+            'user' => $user
+        ], 200);
     }
 
     public function destroy($id)
     {
         User::destroy($id);
-        return 'User deleted.';
+        return response()->json([
+            'ok'=> true,
+            'message' => 'User deleted'
+        ], 200);
     }
 }
